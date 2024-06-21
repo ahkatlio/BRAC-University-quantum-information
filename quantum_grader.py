7519c0e0f0843c1984407d925d5d2c7acdc7c038
Abdullah Al Omar Galib <109025616+ahkatlio@users.noreply.github.com>
2024-06-21 19:48:49 +0600

    Update quantum_grader.py

--- quantum_grader.py
+++ quantum_grader.py
@@ -1,4 +1,7 @@
 import numpy as np
+from qiskit import QuantumCircuit, transpile
+from qiskit_aer import Aer
+from qiskit.quantum_info import Statevector
 import base64
 
 def _obscure_logic(a, b):
@@ -23,3 +26,28 @@ def check_homework_1(user_final_state):
         return _decode_message(correct_msg)
     else:
         return _decode_message(incorrect_msg)
+
+def check_homework_2(student_circuit):
+    init_circuit = QuantumCircuit(1)
+    init_circuit.h(0)
+    backend = Aer.get_backend('statevector_simulator')
+    init_circuit = transpile(init_circuit, backend)
+    job = backend.run(init_circuit, shots=1000)
+    plus_state = job.result().get_statevector()
+    
+    expected_circuit = QuantumCircuit(1)
+    expected_circuit.h(0)  
+    expected_circuit.x(0) 
+
+    expected_circuit = transpile(expected_circuit, backend)
+    job = backend.run(expected_circuit, shots=1000)
+    expected_final_state = job.result().get_statevector()
+    
+    student_circuit = transpile(student_circuit, backend)
+    job = backend.run(student_circuit, shots=1000)
+    student_final_state = job.result().get_statevector()
+    
+    if np.allclose(student_final_state, expected_final_state):
+        return "Correct! Your final state matches the expected state."
+    else:
+        return "Incorrect. Please try again."
